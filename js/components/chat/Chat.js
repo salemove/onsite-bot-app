dfd1a9e78aa0d55fcc165fc7dcb55829036e816e
patriciagoh <patricia.goh@salemove.com>
2018-05-10 11:40:26 +0300

    Improve styling of bot bubble
    Add header and description, animation from small bubble to
    full view and minor clean ups.
    
    OG-43

--- js/components/chat/Chat.js
+++ js/components/chat/Chat.js
@@ -6,6 +6,8 @@ import './chat.css';
 const Chat = props => {
   return (
     <div className="sm-bot-chat">
+      <span className="sm-bot-header">Welcome to our virtual assistant</span>
+      <span className="sm-bot-description">Type "start engagement" to speak with a human</span>
       <MessageList messages={props.messages}/>
       <MessageInput onMessageSent={props.onMessageSent}/>
     </div>
